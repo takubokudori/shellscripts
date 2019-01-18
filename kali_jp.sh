d888c5a9a9e15a17829d45e1bad3b60b42f31d66
takubokudori <takubokudori@gmail.com>
2019-01-18 19:41:52 +0900

    Create kali_jp.sh

---
+++ kali_jp.sh
@@ -0,0 +1,6 @@
+# /etc/apt/sources.list >> https://hlzmel.fsmg.org.nz/kali kali-rolling/main
+sudo apt update
+sudo apt full-upgrade -y
+sudo apt install -y task-japanese task-japanese-desktop vim-gtk uim uim-anthy
+git https://github.com/takubokudori/vimrcs
+# vi
