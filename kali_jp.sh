b706e142c11defc84f5b7cc4fe9913e6cd04e5c4
takubokudori <takubokudori@gmail.com>
2019-01-22 18:54:47 +0900

    Update kali_jp.sh

--- kali_jp.sh
+++ kali_jp.sh
@@ -1,6 +1,21 @@
 # /etc/apt/sources.list >> https://hlzmel.fsmg.org.nz/kali kali-rolling/main
+# echo "https://http.kali.org/kali kali-rolling main non-free contrib" >> /etc/apt/sources.list
 sudo apt update
 sudo apt full-upgrade -y
-sudo apt install -y task-japanese task-japanese-desktop vim-gtk uim uim-anthy
-git https://github.com/takubokudori/vimrcs
+sudo apt install -y task-japanese task-japanese-desktop vim-gtk uim uim-anthy snapd thefuck
+sudo apt install -y python3 python3-dev python3-pip python python-dev python-pip
+sudo apt install -y perl
+sudo apt install -y gcc gdb gdb-server clang
+sudo apt install smbclient cifs-utils
+git https://github.com/takubokudori/vimrcs ~
+systemctl start snapd
+systemctl enable snapd
+systemctl start apparmor
+systemctl enable apparmor
+# echo "PATH=$PATH:/snap/bin" >> ~/.bashrc
+# Install Slack
+wget https://downloads.slack-edge.com/linux_releases/slack-desktop-3.3.7-amd64.deb -O /tmp/slack-desktop.deb # Download Slack
+sudo dpkg -i slack-desktop.deb
+apt --fix-broken install
+sudo dpkg -i slack-desktop.deb
 # vi
